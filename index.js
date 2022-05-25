957744b0fb988660151656fbeba3a13e604b4bdb
Fermin Bugallo <bugallof@gmail.com>
2022-05-25 16:00:30 -0300

    Deployed amost ready

--- index.js
+++ index.js
@@ -25,10 +25,10 @@
 // ⣿⣿⣿⡿⡑⣢⢴⡏⢹⢻⣿⣿⡀⢨⡏⠴⡀⠌⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠛⣽⢿⣿⣿⣿⣿⣯⣧⣿⢢⠀⡌⣿⣿⣾
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-
+const {PORT} = process.env;
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
   });
 });
--- src/db.js
+++ src/db.js
@@ -2,14 +2,22 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DATABASE_URL } = process.env;
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/artpage`,
+  DATABASE_URL,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  }
+    dialectOptions:
+    {
+      ssl:
+      {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
 );
 const basename = path.basename(__filename);
 
@@ -37,7 +45,7 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Artwork, Profile, User, Category } = sequelize.models;
+const { Artwork, Profile, Category } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
