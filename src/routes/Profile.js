d25cd57d64007c45585430ef84d7c58d9e2bda1a
Fermin Bugallo <bugallof@gmail.com>
2022-05-27 12:53:13 -0300

    Change

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -2,6 +2,46 @@ const { Router } = require("express");
 const router = Router();
 const { Profile } = require("../db.js");
 
+const getProfiles = async (req, res) => {
+  try {
+    const { name } = req.query;
+
+    if (!name) {
+      let profiles = await Profile.findAll();
+
+      profiles.map((e) => {
+        // console.log(artWorks)
+        return {
+          id: e.id,
+          name: e.name,
+          lastName: e.lastName,
+          userName: e.userName,
+          email: e.email,
+          password: e.password,
+          day_of_birth: e.day_of_birth,
+          gender: e.gender,
+          is_Admin: e.is_Admin,
+          img: e.img,
+          phone: e.phone,
+          public_email: e.public_email,
+          description: e.description,
+          country: e.country,
+        };
+      });
+      res.status(200).json(profiles);
+    } else {
+      let oneProfile = await Profile.findAll({ where: { name: name } });
+      oneProfile.length
+        ? res.status(200).json(oneProfile)
+        : res.status(404).send("No existe ese perfil.");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+router.get("/", getProfiles);
+
 const postProfile = async (req, res) => {
   try {
     const {
@@ -86,45 +126,7 @@ const putProfile = async (req, res) => {
 
 router.put("/:id", putProfile);
 
-const getProfiles = async (req, res) => {
-  try {
-    const { name } = req.query;
 
-    if (!name) {
-      let profiles = await Profile.findAll();
-
-      profiles.map((e) => {
-        // console.log(artWorks)
-        return {
-          id: e.id,
-          name: e.name,
-          lastName: e.lastName,
-          userName: e.userName,
-          email: e.email,
-          password: e.password,
-          day_of_birth: e.day_of_birth,
-          gender: e.gender,
-          is_Admin: e.is_Admin,
-          img: e.img,
-          phone: e.phone,
-          public_email: e.public_email,
-          description: e.description,
-          country: e.country,
-        };
-      });
-      res.status(200).json(profiles);
-    } else {
-      let oneProfile = await Profile.findAll({ where: { name: name } });
-      oneProfile.length
-        ? res.status(200).json(oneProfile)
-        : res.status(404).send("No existe ese perfil.");
-    }
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-router.get("/", getProfiles);
 
 router.get("/:id", async (req, res) => {
   try {
