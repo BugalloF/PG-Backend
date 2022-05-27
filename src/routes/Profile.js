8c49de45c2b7e0c937ca4fa778b3f9261233bae0
Fermin Bugallo <bugallof@gmail.com>
2022-05-27 10:34:00 -0300

    GET/Profile c nombre(opcional)

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -58,7 +58,6 @@ const putProfile = async (req, res) => {
       public_email,
       description,
       country,
-      
     } = req.body;
     let updatedProfile = await Profile.findOne({
       where: {
@@ -77,15 +76,53 @@ const putProfile = async (req, res) => {
       phone,
       public_email,
       description,
-      country
-      
+      country,
     });
     res.status(201).json(updatedProfile);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 };
 
 router.put("/:id", putProfile);
 
+const getProfiles = async (req, res) => {
+  try {
+    const { name } = req.query;
+
+    if (!name) {
+      let profiles = await Profile.findAll();
+
+      profiles.map((e) => {
+        // console.log(artWorks)
+        return {
+          id: e.id,
+          name: e.name,
+          lastName: e.lastName,
+          userName: e.userName,
+          email: e.email,
+          password: e.password,
+          day_of_birth: e.day_of_birth,
+          gender: e.gender,
+          is_Admin: e.is_Admin,
+          img: e.img,
+          phone: e.phone,
+          public_email: e.public_email,
+          description: e.description,
+          country: e.country,
+        };
+      });
+      res.status(200).json(profiles);
+    } else {
+      let oneProfile = await Profile.findAll({ where: { name: name } });
+      oneProfile.length
+        ? res.status(200).json(oneProfile)
+        : res.status(404).send("No existe ese perfil.");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+router.get("/", getProfiles);
 module.exports = router;
