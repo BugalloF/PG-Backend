b7d2cc35dbd419bea1642a6f6fc2804355de1716
jeronimo olivera <omegablak17@gmail.com>
2022-05-31 11:46:40 -0300

    'f'

--- src/db.js
+++ src/db.js
@@ -2,41 +2,35 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-// const {
-//   DB_USER, DB_PASSWORD, DB_HOST,DB_NAME
-// } = process.env;
 
-DB_USER='postgres',
-DB_PASSWORD='Pediter105070',
-DB_HOST='localhost',
-DB_NAME= 'artpage',
+
 
 
 console.log(DB_USER, DB_PASSWORD, DB_HOST)
-// const { DATABASE_URL } = process.env;
+const { DATABASE_URL } = process.env;
 
-// const sequelize = new Sequelize(
-//   DATABASE_URL,
-//   {
-//     logging: false, // set to console.log to see the raw SQL queries
-//     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-//     dialectOptions:
-//     {
-//       ssl:
-//       {
-//         require: true,
-//         rejectUnauthorized: false,
-//       },
-//     },
-//   },
-// );
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+  DATABASE_URL,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  }
+    dialectOptions:
+    {
+      ssl:
+      {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
 );
+// const sequelize = new Sequelize(
+//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+//   {
+//     logging: false, // set to console.log to see the raw SQL queries
+//     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+//   }
+// );
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
