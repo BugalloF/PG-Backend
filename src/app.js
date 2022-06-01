a0026ede169abdd37f58451003cf0e5358c824ec
Fermin Bugallo <bugallof@gmail.com>
2022-06-01 12:07:58 -0300

    force:false && arreglado

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
+const path = require("path");
 
 require('./db.js');
 
