2005597e1b814567e25fa4a519e259cb7e615cea
Fermin Bugallo <bugallof@gmail.com>
2022-06-01 13:48:13 -0300

    multer

--- src/multer/multer.js
+++ src/multer/multer.js
@@ -1,7 +1,12 @@
 const multer = require('multer')
-
 const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
     
+    
+        if(file.fieldname === 'compress') cb(null,  './store')
+        else cb(null,  'store')
+        
+    },
     filename: function (req, file, cb) {
     
          
