93f3b2e4c6749174cc9ae973f13ccc9c5b512bc1
Fermin Bugallo <bugallof@gmail.com>
2022-06-03 17:25:53 -0300

    aaa

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,18 +3,29 @@ const{ getStorage, uploadBytes, ref, getDownloadURL}= require("firebase/storage"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Fi    rebase configuration
+// Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDqDRbXhnoCVAPwETw6pZVwY1gjbBgGYoY",
+//   authDomain: "prueba-408d2.firebaseapp.com",
+//   projectId: "prueba-408d2",
+//   storageBucket: "prueba-408d2.appspot.com",
+//   messagingSenderId: "939713514095",
+//   appId: "1:939713514095:web:a40987f108700af0eb9b9a",
+//   measurementId: "G-6DSZ4TXS62",
+  
+// };
+// ---fer
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDqDRbXhnoCVAPwETw6pZVwY1gjbBgGYoY",
-  authDomain: "prueba-408d2.firebaseapp.com",
-  projectId: "prueba-408d2",
-  // storageBucket: "prueba-408d2.appspot.com",
-  messagingSenderId: "939713514095",
-  appId: "1:939713514095:web:a40987f108700af0eb9b9a",
-  measurementId: "G-6DSZ4TXS62",
-  storageBucket: "artpage.herokuapp.com",
-}; 
+  apiKey: "AIzaSyCZUOmPIV7UgNPqnldUw1N0UDMEeRF_Cg4",
+  authDomain: "artpage-aa77e.firebaseapp.com",
+  projectId: "artpage-aa77e",
+  storageBucket: "artpage-aa77e.appspot.com",
+  messagingSenderId: "209432909419",
+  appId: "1:209432909419:web:8d5032f81b278fcbeb032e"
+};
+
 // const firebaseConfig = {
 
 //   apiKey: "AIzaSyD6SlIY838LFqMVEnJkWb8ny3KxtqhzobU",
@@ -35,4 +46,4 @@ const storage = getStorage(app)
 
 // Initialize Firebase
 module.exports = {storage,uploadBytes,ref, getDownloadURL}
-// const analytics = getAnalytics(app);
+const analytics = getAnalytics(app);
