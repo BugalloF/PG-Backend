25fddd16e763578164084c10f1a7f1f176c7ece4
jeronimo olivera <omegablak17@gmail.com>
2022-06-03 20:20:45 -0300

    'aber'

--- index.js
+++ index.js
@@ -26,7 +26,7 @@
 // ⣿⣿⣿⡿⡑⣢⢴⡏⢹⢻⣿⣿⡀⢨⡏⠴⡀⠌⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠛⣽⢿⣿⣿⣿⣿⣯⣧⣿⢢⠀⡌⣿⣿⣾
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const PORT = process.env
+const{ PORT} = process.env
 // Syncing all the models at once.
 conn.sync({force:false}).then(() => {
   server.listen(PORT/*3001*/, () => {
