b7b2501ea8c493e790ae0b77be21185c167a0559
Fermin Bugallo <bugallof@gmail.com>
2022-06-03 16:46:03 -0300

    hola

--- src/routes/Artworks.js
+++ src/routes/Artworks.js
@@ -128,19 +128,19 @@ const postArtWork = async (req, res, next) => {
     path.join(__dirname, `../multer/original/${req.files.original[0].filename}`)
     );
     // console.log('ORIGINAL',readFileOriginal)
-    console.log('HASTA ACA LLEGA')
-
-  const imageRefOriginal = ref(
-    storage,
-    `images/original/${req.files.original[0].filename}`
+    
+    const imageRefOriginal = ref(
+      storage,
+      `images/original/${req.files.original[0].filename}`
   );
-  console.log('IMG REF ORIGINAL',imageRefOriginal)
+  // console.log('IMG REF ORIGINAL',imageRefOriginal)
 
+      console.log('HASTA ACA LLEGA')
   const uploadImageCompress = await uploadBytes(
     imageRefCompress,
     readFileCompress
-  );
-  console.log(uploadImageCompress)
+  ).then((snapshot)=>console.log('UPLOAD',snapshot));
+  // console.log(uploadImageCompress)
   // console.log('zzzzzzzzzzzz')
   const urlCompress = await getDownloadURL(uploadImageCompress.ref);
     console.log('URLCOMPRESS',urlCompress)
