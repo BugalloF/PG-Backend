89d928fd207ca80a5cf78f9e9a80a638eb9440fd
jeronimo olivera <omegablak17@gmail.com>
2022-06-04 17:21:57 -0300

    'email'

--- src/routes/Filters.js
+++ src/routes/Filters.js
@@ -51,14 +51,10 @@ router.get("/category", async (req, res,next) => {
   // Ahora lo hacemos como se tinee que hacer :
   try {
     let Artworks = await Artwork.findAll({
-      include: {
-        model: Category,
-        where: {
-          title: category,
-        },
-        limit: 12,
-        offset: from * 12,
-      },
+      include: [{
+        model: Profile,
+        where: { title: category } 
+      }],
       attributes:['imgCompress','id','likes','price','title'],
     });
     let counter = await Artwork.count();
