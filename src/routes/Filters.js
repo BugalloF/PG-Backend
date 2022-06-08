34f7a8f481b872d87971a2eb327007a8d80f0422
jeronimo olivera <omegablak17@gmail.com>
2022-06-08 15:06:13 -0300

    'firtcommit'

--- src/routes/Filters.js
+++ src/routes/Filters.js
@@ -6,9 +6,13 @@ const {Artwork, Category, Profile} = require("../db.js");
 
 
 router.get("/category", async (req, res,next) => {
-  const { category, from =0} = req.query;
-  
+
+  const { category, from =0, by, type } = req.query;
+  var order = null
+  if(by && type) order = 'order'
+ 
   try {
+
     let Artworks = await Artwork.findAll({
       include: [{
         model: Category,
@@ -18,15 +22,20 @@ router.get("/category", async (req, res,next) => {
           attributes: [],
         }, 
       }],
+      [order]: [by, type],
       limit: 12,
       offset: from * 12,
       attributes:['imgCompress','id','likes','price','title'],
     });
+
     let counter = await Artwork.count();
+
     res.status(200).json({Artworks,counter});
     
   } catch (error) {
+
     next(error)
+
   }
 });
 // -------------------------------- ORDENAMIENTOS --------------------
