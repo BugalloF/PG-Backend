9965cb3a59f7bc2215c3314b73f7c74e00be5d8c
jeronimo olivera <omegablak17@gmail.com>
2022-06-13 01:24:41 -0300

    'deleteandputcategories'

--- src/routes/Artworks.js
+++ src/routes/Artworks.js
@@ -8,6 +8,20 @@ const {verifyToken} = require("../controllers/tokens");
 const {API_KEY} = process.env;
 
 
+const countArtworks = async (req,res,next) =>{
+  try{
+    const count = await Artwork.count();
+
+    res.status(200).json(count)
+
+  }catch(error){
+    next(error)
+  }
+}
+
+router.get('/count',countArtworks)
+
+
 
 // --------------------------GET-------------------------------- //
 
@@ -305,17 +319,5 @@ router.delete("/likes/:id", async (req, res, next) => {
   }
 });
 
-const countArtworks = async (req,res,next) =>{
-  try{
-    const count = await Artwork.count();
-
-    res.status(200).json(count)
-
-  }catch(error){
-    next(error)
-  }
-}
-
-router.get('/count',countArtworks)
 
 module.exports = router;
