83cc5a99391426ff80262e08aab5c430b6f039b3
Juliana Carreño <julimaschi@gmail.com>
2022-06-14 09:43:43 -0300

    agregando email vendedor

--- src/routes/sendMail.js
+++ src/routes/sendMail.js
@@ -20,13 +20,15 @@ router.post("/send-email", async (req, res) => {
         
         if(tokenEmail)
         {
-            const {payer, surname, time, idPost} = req.body;
+            // const {payer,surname,time,idPost} = req.body;
+            const {payer,surname,time,idPost,userSeller,userPayer,email} = req.body;
             const art = await Artwork.findByPk(idPost)
             
             const image = art.dataValues.img
             const title = art.dataValues.title
+            const price = art.dataValues.price
             
-            const contentHtml = `
+            const contentHtmlPayer = `
                 <!DOCTYPE html>
                 <html>
                 <head>
@@ -35,10 +37,34 @@ router.post("/send-email", async (req, res) => {
                 <title>Compra Email</title>
                 </head>
                 <body>
-                <h2>Hola ${payer} ${surname}!</h2>
+                <h2>Hola ${userPayer}!</h2>
                 <p>Gracias por tu compra en DigitalizArte.</p>
                 <p>Obra: ${title}</p>
                 <p>Hora de pago: ${time}</p>
+                <br></br>
+                <h5>Gracias por utilizar DigitalizArte.</h5>
+                </body>
+                </html>
+                `
+            ;
+            const contentHtmlSeller = `
+                <!DOCTYPE html>
+                <html>
+                <head>
+                <meta charset="utf-8">
+                <meta http-equiv="x-ua-compatible" content="ie=edge">
+                <title>Venta Email</title>
+                </head>
+                <body>
+                <h2>Hola ${userSeller}!</h2>
+                <p>Acabamos de registrar una nueva venta de una de tus obras!.</p>
+                <br></br>
+                <h5>Información de venta: </h5>
+                <p>Obra vendida: ${title}</p>
+                <p>Hora de pago: ${time}</p>
+                <p>Precio: ${price}</p>
+                <br></br>
+                <h5>Gracias por utilizar DigitalizArte! en las próximas horas se acreditará el dinero en tu cuenta.</h5>
                 </body>
                 </html>
                 `
@@ -47,7 +73,7 @@ router.post("/send-email", async (req, res) => {
             const oAuth2Client = new google.auth.OAuth2(CLIENT_ID , CLIENT_SECRET , REDIRECT_URI);
             oAuth2Client.setCredentials({refresh_token: REFRESH_TOKEN});
             
-            async function sendMail(){
+            async function sendMailPayer(){
                 try{
                     const accessToken = await oAuth2Client.getAccessToken();
                     const transporter = nodemailer.createTransport({
@@ -64,15 +90,45 @@ router.post("/send-email", async (req, res) => {
                     const mailOptions = {
                         from: '"DigitalizArte" <digitalizartecompras@gmail.com>',
                         to: tokenEmail,
-                        subject: "Compra en DigitalizArte",
-                        html: contentHtml,
+                        subject: "compra en DigitalizArte",
+                        html: contentHtmlPayer,
                         attachments: [{
                             filename: `${title}.jpg`,
                             path: image,
                         }],
                     };
                     
-                    const result = await transporter.sendMail(mailOptions);
+                    const result = await transporter.sendMailPayer(mailOptions);
+                    return result;
+                }
+                catch(err)
+                {
+                    console.log(err.message);
+                };
+            };
+
+            async function sendMailSeller(){
+                try{
+                    const accessToken = await oAuth2Client.getAccessToken();
+                    const transporter = nodemailer.createTransport({
+                        service:"gmail",
+                        auth: {
+                            type: "OAuth2",
+                            user: "digitalizartecompras@gmail.com",
+                            clientId : CLIENT_ID,
+                            clientSecret : CLIENT_SECRET,
+                            refreshToken : REFRESH_TOKEN,
+                            accessToken: accessToken,
+                        },
+                    });
+                    const mailOptions = {
+                        from: '"DigitalizArte" <digitalizartecompras@gmail.com>',
+                        to: email,
+                        subject: "Venta en DigitalizArte",
+                        html: contentHtmlSeller
+                    };
+                    
+                    const result = await transporter. sendMailSeller(mailOptions);
                     return result;
                 }
                 catch(err)
@@ -81,8 +137,11 @@ router.post("/send-email", async (req, res) => {
                 };
             };
             
-            sendMail()
+            sendMailPayer()
                 .then(result => res.status(200).send("ENVIADOO"))
+                .catch((error)=>console.log(error.message));
+            sendMailSeller()
+                .then(result => res.status(200).send("ENVIADOO 2"))
                 .catch((error)=>console.log(error.message));
         }
         else
